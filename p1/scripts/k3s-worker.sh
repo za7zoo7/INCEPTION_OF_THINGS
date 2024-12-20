5d5fe55b1806c9bf7313ca0d1faffce574668d9d
za7adotexe <XXXXX@breakandpray.com>
2024-12-20 14:28:09 +0100

    correct worker node ip

--- p1/scripts/k3s-worker.sh
+++ p1/scripts/k3s-worker.sh
@@ -6,4 +6,4 @@ MASTER_IP=$1
 TOKEN=$(cat /vagrant/token)
 
 # Install K3s agent (worker) and join the master node
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip 192.168.42.110" K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip 192.168.42.111" K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
