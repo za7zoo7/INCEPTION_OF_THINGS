c5b01dae7bd3a62ddc76c72c3b1d2b27842f2574
za7adotexe <XXXXX@breakandpray.com>
2024-12-20 14:24:15 +0100

    fixing k3s config permission problem

--- p1/scripts/k3s-master.sh
+++ p1/scripts/k3s-master.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # Install K3s on the master node
-curl -sfL https://get.k3s.io | sh -
-
-# Make sure kubectl is set up for the vagrant user
-sudo mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip 192.168.42.110" sh -
 
 # Get the token for the worker nodes
 TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
