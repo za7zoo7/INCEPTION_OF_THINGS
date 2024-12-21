0ef01190f66264fc71ae1589609fce9e0c822401
za7adotexe <XXXXX@breakandpray.com>
2024-12-21 05:22:52 +0100

    fix vm's name

--- p1/scripts/k3s-worker.sh
+++ p1/scripts/k3s-worker.sh
@@ -7,3 +7,6 @@ TOKEN=$(cat /vagrant/token)
 
 # Install K3s agent (worker) and join the master node
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip 192.168.42.111" K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+
+# Clean up the token
+rm /vagrant/token
